dbad3c3a5c633cbd91b6d4a8f1fcfeacd6ed6da7
opfx <bogdan@opfx.org>
2017-11-19 12:58:26 -0500

    Made core a module

--- src/main/php/hermes/Application.php
+++ src/main/php/hermes/Application.php
@@ -3,12 +3,12 @@
 namespace hermes;
 
 use Object;
-use hermes\internal\service\Gateway;
+use hermes\internal\service\HttpGateway;
 
 class Application extends Object {
 
 	static public function main(): void {
-		$gateway = new Gateway();
+		$gateway = new HttpGateway();
 		$gateway->service();
 	}
 }
