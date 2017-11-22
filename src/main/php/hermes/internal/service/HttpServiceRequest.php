3b3d5585ae3cd5a2044228453ff89ff6183935d9
opfx <bogdan@opfx.org>
2017-11-22 16:31:37 -0500

    Reading json data from php input stream

--- src/main/php/hermes/internal/service/HttpServiceRequest.php
+++ src/main/php/hermes/internal/service/HttpServiceRequest.php
@@ -15,16 +15,20 @@ class HttpServiceRequest extends HTTPRequest implements IServiceRequest {
 		$strUrl = "{$_SERVER['REQUEST_SCHEME']}://{$_SERVER['SERVER_NAME']}:{$_SERVER['SERVER_PORT']}{$_SERVER['REQUEST_URI']}";
 		$url = new URL( $strUrl );
 		$request->setUrl( $url );
-		$request->setContentType( self::getContentType() );
-		if ( isset( $_REQUEST['json'] ) ) {
-			$request->setBody( $_REQUEST['json'] );
-		}
+		$request->setMethod( $_SERVER['REQUEST_METHOD'] );
+		$request->setContentType( self::getRequestContentType() );
 
 		if ( $request->getContentType() == 'application/json' ) {
-			$json = $request->getBody();
-			$data = json_decode( $json );
+			if ( isset( $_REQUEST['json'] ) ) {
+				$request->setBody( $_REQUEST['json'] );
+			}
+			$rawPostData = file_get_contents( 'php://input' );
+			if ( ! empty( $rawPostData ) ) {
+				$data = json_decode( $rawPostData );
+			}
+
 			$properties = get_object_vars( $data );
-			foreach ( $properties as $name->$value ) {
+			foreach ( $properties as $name => $value ) {
 				$request->setParameter( $name, $value );
 			}
 		}
@@ -45,7 +49,7 @@ class HttpServiceRequest extends HTTPRequest implements IServiceRequest {
 			return $_REQUEST['contentType'];
 		}
 		if ( isset( $_SERVER['CONTENT_TYPE'] ) ) {
-			return $_REQUEST['CONTENT_TYPE'];
+			return $_SERVER['CONTENT_TYPE'];
 		}
 		return $contentType;
 	}
