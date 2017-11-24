e000dace7d01aed82eed0db7c48b2c10efb29b72
opfx <bogdan@opfx.org>
2017-11-24 01:40:02 -0500

    Some progress

--- index.php
+++ index.php
@@ -1,14 +1,16 @@
 <?php
-use hermes\Application;
+require_once 'mockrequest.php';
 
-$val = null;
-$t = json_decode( 'null' );
-
-$e = $t;
-
-$_SERVER['REQUEST_URI'] = '/api/v1/content/items';
+$_SERVER = array_merge( $_SERVER, $MOCK_SERVER );
+$_GET = array_merge( $_GET, $MOCK_GET );
+$_POST = array_merge( $_POST, $MOCK_POST );
+$_REQUEST = array_merge( $_GET, $_POST );
+$_COOKIE = array_merge( $_COOKIE, $MOCK_COOKIE );
+$_COOKIE['debug_host'] = '127.0.0.1';
 
 require_once '../opfx/dist/opfx.phar';
 require_once 'dist/core.phar';
+
+use hermes\Application;
 
 Application::main();
---
+++ mockrequest.php
@@ -0,0 +1,63 @@
+<?php
+//@formatter:off
+$MOCK_SERVER = array (
+		'INSTALLATION_UID' => '1115172240',
+		'ZEND_TMPDIR' => '/usr/local/zend/tmp',
+		'USER' => 'nobody',
+		'HOME' => '/',
+		'HTTP_COOKIE' => 'ZS6SESSID=f8cb9f5026ccf026492c4b79911a1b9f; ZSDEVBAR=%7B%7D',
+		'HTTP_ACCEPT_LANGUAGE' => 'en-US,en;q=0.9',
+		'HTTP_ACCEPT_ENCODING' => 'gzip, deflate',
+		'HTTP_REFERER' => 'http://hermes.bt.opfx.org/sb/ServiceBrowser.php',
+		'HTTP_CONTENT_TYPE' => 'application/x-www-form-urlencoded',
+		'HTTP_USER_AGENT' => 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0...cut...',
+		'HTTP_ZRAY_ID' => '3@8345@1511300892@0',
+		'HTTP_X_REQUESTED_WITH' => 'XMLHttpRequest',
+		'HTTP_ORIGIN' => 'http://hermes.bt.opfx.org',
+		'HTTP_ACCEPT' => 'application/json, text/javascript, */*; q=0.01',
+		'HTTP_CONTENT_LENGTH' => '80',
+		'HTTP_HOST' => 'hermes.bt.opfx.org',
+		'REDIRECT_STATUS' => '200',
+		'SERVER_NAME' => 'hermes.bt.opfx.org',
+		'SERVER_PORT' => '80',
+		'SERVER_ADDR' => '172.31.18.48',
+		'REMOTE_PORT' => '28928',
+		'REMOTE_ADDR' => '204.50.157.66',
+		'SERVER_SOFTWARE' => 'nginx/1.12.2',
+		'GATEWAY_INTERFACE' => 'CGI/1.1',
+		'REQUEST_SCHEME' => 'http',
+		'SERVER_PROTOCOL' => 'HTTP/1.1',
+		'DOCUMENT_ROOT' => '/usr/local/zend/var/apps/http/hermes.bt.opfx.org/80/_docroot_',
+		'DOCUMENT_URI' => '/index.php',
+		'REQUEST_URI' => '/index.php?contentType=application/json',
+		'SCRIPT_NAME' => '/index.php',
+		'SCRIPT_FILENAME' => '/usr/local/zend/var/apps/http/hermes.bt.opfx.org/80/_docroot_/index.php',
+		'CONTENT_LENGTH' => '80',
+		'CONTENT_TYPE' => 'application/x-www-form-urlencoded',
+		'REQUEST_METHOD' => 'POST',
+		'QUERY_STRING' => 'contentType=application/json',
+		'FCGI_ROLE' => 'RESPONDER',
+		'PHP_SELF' => '/index.php',
+		'REQUEST_TIME_FLOAT' => 1511379201.74637508392333984375,
+		'REQUEST_TIME' => 1511379201,
+);
+
+$MOCK_GET = array (
+		'contentType' => 'application/json',
+);
+
+$MOCK_POST = array (
+		'{"serviceName":"DiscoveryService","methodName":"discover","parameters":' =>
+		array (
+				0 => '',
+		),
+);
+
+$MOCK_COOKIE = array (
+		'ZS6SESSID' => 'f8cb9f5026ccf026492c4b79911a1b9f',
+		'ZSDEVBAR' => '{}',
+);
+
+$MOCK_RAWPOST = '{"serviceName":"DiscoveryService","methodName":"discover","parameters":[]}';
+
+//@formatter:on
