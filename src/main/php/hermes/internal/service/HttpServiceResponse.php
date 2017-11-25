2b2cbc791f944425693528bbaa47eec78a738e7c
opfx <bogdan@opfx.org>
2017-11-24 19:42:49 -0500

    Removed IServiceRequest and IServiceResponse

--- src/main/php/hermes/internal/service/HttpServiceResponse.php
+++ src/main/php/hermes/internal/service/HttpServiceResponse.php
@@ -3,7 +3,6 @@
 namespace hermes\internal\service;
 
 use opfx\net\http\HTTPResponse;
-use hermes\service\IServiceResponse;
 
 class HttpServiceResponse extends HttpResponse {
 }
--- src/main/php/hermes/service/IServiceResponse.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-namespace hermes\service;
-
-interface IServiceResponse {
-}
